e48093de6268789d57aaf43a95fa1a4aa9ed0c56
Banshee <657509220@qq.com>
2017-10-11 21:01:51 +0800

    refine

--- regEngine/regEngine/Edge.h
+++ regEngine/regEngine/Edge.h
@@ -2,17 +2,17 @@
 
 #include <vector>
 #include "State.h"
-enum { LCASES=256, UCASES=257, NUM=258, EPSILON=259, ANY=260, WS=262 };
-enum { EXCLUDED = true, NEXCLUDED = false};
+enum CHARTYPE{ LCASES=256, UCASES=257, NUM=258, EPSILON=259, ANY=260, WS=262 };
+enum IFEXCLUDE{ EXCLUDED = true, NEXCLUDED = false};
 
 class Edge
 {
 public:
-	Edge(State *start, State *end, int type, bool exclude = NEXCLUDED);
+	Edge(State *start, State *end, int type, IFEXCLUDE exclude = NEXCLUDED);
 	State *start;
 	State *end;
-	int type;
-	bool exclude;
+	CHARTYPE type;
+	IFEXCLUDE exclude;
 
 	bool match(char c);
 	State *stepNext();
