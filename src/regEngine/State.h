4a1c1218081949e402d23ff7eebabd394af65878
Banshee <657509220@qq.com>
2017-10-16 10:42:48 +0800

    change the file name

---
+++ src/regEngine/State.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef STATE_H
+#define STATE_H
+
+#include<vector>
+#include"Edge.h"
+#include "Nfa.h"
+
+using namespace std;
+enum { READY = -1, SUCCESS = 1, FAIL = 0};
+
+class State
+{
+public:
+	int status;
+	std::vector<Edge *> InEdges;
+	std::vector<Edge *> OutEdges;
+
+public:
+	State();
+	State(Edge *InEdge, Edge *OutEdge, int status);
+	void merge(State *s);
+	void patch(Edge *e, State *s);
+	void patch(State *s, Edge *e);
+};
+
+#endif
+
